84c01aa64c98bbc967529c52affdc11fd5f4ee23
mhlengi <khambulek03@gmail.com>
2025-12-31 21:22:40 +0200

    Just added two entities customer and wallet

---
+++ src/main/java/com/techtitans/onlineshop/domain/Wallet.java
@@ -0,0 +1,35 @@
+package com.techtitans.onlineshop.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "wallet")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Wallet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Min(value = 0)
+    private Double balance;
+
+    private UUID customerId;
+
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    private void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
