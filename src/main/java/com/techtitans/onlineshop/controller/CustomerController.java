34ae4ca421df1d99112ef64af9bb04c0d0c9a1a9
Ntandoyenkosi Mncube <khambulek03@gmail.com>
2026-07-14 09:16:09 +0200

    Update CustomerController.java

--- src/main/java/com/techtitans/onlineshop/controller/CustomerController.java
+++ src/main/java/com/techtitans/onlineshop/controller/CustomerController.java
@@ -1,8 +1,5 @@
 package com.techtitans.onlineshop.controller;
 
-import com.techtitans.onlineshop.domain.Customer;
-import com.techtitans.onlineshop.dto.CustomerDataRequest;
-import com.techtitans.onlineshop.service.CustomerService;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -14,12 +11,6 @@ import static org.springframework.http.HttpStatus.OK;
 @RestController
 @RequestMapping(path = "api/products")
 public class CustomerController {
-
-    private final CustomerService customerService;
-
-    public CustomerController(CustomerService customerService) {
-        this.customerService = customerService;
-    }
 
     @GetMapping("/")
     public String greetings() {
